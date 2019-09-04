25b55e660c91ddc21955e4ad68c2db546827ce5f
Ilfat Galiev <v-ilgali@microsoft.com>
2019-09-04 12:48:53 +0300

    Migrate the visual to tools 3.x.x

--- src/visual.ts
+++ src/visual.ts
@@ -1,114 +1,122 @@
 /*
- *  Power BI Visual CLI
- *
- *  Copyright (c) Microsoft Corporation
- *  All rights reserved.
- *  MIT License
- *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the ""Software""), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
- *
- *  The above copyright notice and this permission notice shall be included in
- *  all copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- *  THE SOFTWARE.
- */
+*  Power BI Visual CLI
+*
+*  Copyright (c) Microsoft Corporation
+*  All rights reserved.
+*  MIT License
+*
+*  Permission is hereby granted, free of charge, to any person obtaining a copy
+*  of this software and associated documentation files (the ""Software""), to deal
+*  in the Software without restriction, including without limitation the rights
+*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*  copies of the Software, and to permit persons to whom the Software is
+*  furnished to do so, subject to the following conditions:
+*
+*  The above copyright notice and this permission notice shall be included in
+*  all copies or substantial portions of the Software.
+*
+*  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+*  THE SOFTWARE.
+*/
 
-module powerbi.extensibility.visual {
-    "use strict";
-    export class Visual implements IVisual {
-        private host: IVisualHost;
-        private svg: d3.Selection<SVGElement>;
-        private container: d3.Selection<SVGElement>;
-        private circle: d3.Selection<SVGElement>;
-        private textValue: d3.Selection<SVGElement>;
-        private textLabel: d3.Selection<SVGElement>;
+"use strict";
+import "core-js/stable";
+import "../style/visual.less";
+import powerbi from "powerbi-visuals-api";
+import IVisual = powerbi.extensibility.IVisual;
+import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
+import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
+import IVisualHost = powerbi.extensibility.visual.IVisualHost;
+import DataView = powerbi.DataView;
+import EnumerateVisualObjectInstancesOptions = powerbi.EnumerateVisualObjectInstancesOptions;
+import VisualObjectInstanceEnumeration = powerbi.VisualObjectInstanceEnumeration;
+import { VisualSettings } from "./settings";
 
-        private visualSettings: VisualSettings;
+import * as d3 from "d3";
 
-        constructor(options: VisualConstructorOptions) {
-            this.svg = d3.select(options.element)
-                .append('svg')
-                .classed('circleCard', true);
-            this.container = this.svg.append("g")
-                .classed('container', true);
-            this.circle = this.container.append("circle")
-                .classed('circle', true);
-            this.textValue = this.container.append("text")
-                .classed("textValue", true);
-            this.textLabel = this.container.append("text")
-                .classed("textLabel", true);
-        }
+type Selection<T extends d3.BaseType> = d3.Selection<T, any,any, any>;
 
-        public update(options: VisualUpdateOptions) {
-            //this.settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0]);
+export class Visual implements IVisual {
+    private host: IVisualHost;
+    private svg: Selection<SVGElement>;
+    private container: Selection<SVGElement>;
+    private circle: Selection<SVGElement>;
+    private textValue: Selection<SVGElement>;
+    private textLabel: Selection<SVGElement>;
 
-            let dataView: DataView = options.dataViews[0];
+    private visualSettings: VisualSettings;
 
-            this.visualSettings = VisualSettings.parse<VisualSettings>(dataView);
-            this.visualSettings.circle.circleThickness = Math.max(0, this.visualSettings.circle.circleThickness);
-            this.visualSettings.circle.circleThickness = Math.min(10, this.visualSettings.circle.circleThickness);
+    constructor(options: VisualConstructorOptions) {
+        this.svg = d3.select(options.element)
+            .append('svg')
+            .classed('circleCard', true);
+        this.container = this.svg.append("g")
+            .classed('container', true);
+        this.circle = this.container.append("circle")
+            .classed('circle', true);
+        this.textValue = this.container.append("text")
+            .classed("textValue", true);
+        this.textLabel = this.container.append("text")
+            .classed("textLabel", true);
+    }
+
+    public update(options: VisualUpdateOptions) {
+        //this.settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0]);
+
+        let dataView: DataView = options.dataViews[0];
+
+        this.visualSettings = VisualSettings.parse<VisualSettings>(dataView);
+        this.visualSettings.circle.circleThickness = Math.max(0, this.visualSettings.circle.circleThickness);
+        this.visualSettings.circle.circleThickness = Math.min(10, this.visualSettings.circle.circleThickness);
 
 
-            let width: number = options.viewport.width;
-            let height: number = options.viewport.height;
-            this.svg.attr({
-                width: width,
-                height: height
-            });
-            let radius: number = Math.min(width, height) / 2.2;
-            this.circle
-                .style("fill", this.visualSettings.circle.circleColor)
-                .style("fill-opacity", 0.5)
-                .style("stroke", "black")
-                .style("stroke-width", this.visualSettings.circle.circleThickness)
-                .attr({
-                    r: radius,
-                    cx: width / 2,
-                    cy: height / 2
-                });
-            let fontSizeValue: number = Math.min(width, height) / 5;
-            this.textValue
-                .text(dataView.single.value as string)
-                .attr({
-                    x: "50%",
-                    y: "50%",
-                    dy: "0.35em",
-                    "text-anchor": "middle"
-                }).style("font-size", fontSizeValue + "px");
-            let fontSizeLabel: number = fontSizeValue / 4;
-            this.textLabel
-                .text(dataView.metadata.columns[0].displayName)
-                .attr({
-                    x: "50%",
-                    y: height / 2,
-                    dy: fontSizeValue / 1.2,
-                    "text-anchor": "middle"
-                }).style("font-size", fontSizeLabel + "px");
-        }
+        let width: number = options.viewport.width;
+        let height: number = options.viewport.height;
+        this.svg.attr("width", width);
+        this.svg.attr("height", height);
+        let radius: number = Math.min(width, height) / 2.2;
+        this.circle
+            .style("fill", this.visualSettings.circle.circleColor)
+            .style("fill-opacity", 0.5)
+            .style("stroke", "black")
+            .style("stroke-width", this.visualSettings.circle.circleThickness)
+            .attr("r", radius)
+            .attr("cx", width / 2)
+            .attr("cy", height / 2);
+        let fontSizeValue: number = Math.min(width, height) / 5;
+        this.textValue
+            .text(<string>dataView.single.value)
+            .attr("x", "50%")
+            .attr("y", "50%")
+            .attr("dy", "0.35em")
+            .attr("text-anchor", "middle")
+            .style("font-size", fontSizeValue + "px");
+        let fontSizeLabel: number = fontSizeValue / 4;
+        this.textLabel
+            .text(dataView.metadata.columns[0].displayName)
+            .attr("x", "50%")
+            .attr("y", height / 2)
+            .attr("dy", fontSizeValue / 1.2)
+            .attr("text-anchor", "middle")
+            .style("font-size", fontSizeLabel + "px");
+    }
 
-        private static parseSettings(dataView: DataView): VisualSettings {
-            return VisualSettings.parse(dataView) as VisualSettings;
-        }
+    private static parseSettings(dataView: DataView): VisualSettings {
+        return <VisualSettings>VisualSettings.parse(dataView);
+    }
 
-        /** 
-         * This function gets called for each of the objects defined in the capabilities files and allows you to select which of the 
-         * objects and properties you want to expose to the users in the property pane.
-         * 
-         */
-        public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstanceEnumeration {
-            const settings: VisualSettings = this.visualSettings || VisualSettings.getDefault() as VisualSettings;
-            return VisualSettings.enumerateObjectInstances(settings, options);
-        }
+    /** 
+     * This function gets called for each of the objects defined in the capabilities files and allows you to select which of the 
+     * objects and properties you want to expose to the users in the property pane.
+     * 
+     */
+    public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstanceEnumeration {
+        const settings: VisualSettings = this.visualSettings || <VisualSettings>VisualSettings.getDefault();
+        return VisualSettings.enumerateObjectInstances(settings, options);
     }
 }
